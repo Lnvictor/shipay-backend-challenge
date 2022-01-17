979d9956c6d22333d54307cce5e0e98e1a84e50c
Lnvictor <lnvictor@github.com>
2022-01-17 08:21:31 -0300

    Changes

--- select.sql
+++ select.sql
@@ -1,11 +1,6 @@
-select
-cc.id,
-cc.name,
-cc.email,
-roles.description,
-ca.description as claim_description
-FROM  
-
-users cc INNER JOIN roles ON cc.role_id = roles.id
-INNER JOIN user_claims uc on cc.id  = uc.user_id 
-INNER join claims ca on ca.id = uc.claim_id 
+SELECT cc.id, cc.name, cc.email, roles.description, ca.description AS claim_description
+    FROM  users cc 
+    INNER JOIN roles ON cc.role_id = roles.id
+    INNER JOIN user_claims uc on cc.id  = uc.user_id 
+    INNER join claims ca ON ca.id = uc.claim_id
+WHERE cc.id = 1 -- Insira aqui o Id do usuário que você quer consultar
